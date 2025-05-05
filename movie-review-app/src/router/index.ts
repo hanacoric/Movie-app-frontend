99a657045d5bbe753edc66df38761dd220dfa8df
hanacoric <hancor01@easv365.dk>
2025-05-05 23:02:52 +0200

    initial commit

---
+++ movie-review-app/src/router/index.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/home.vue";
+
+const routes = [{ path: "/", name: "Home", component: Home }];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
